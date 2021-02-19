11a4c6c4f8affd480c3ed26c2bae2c22ed7fd96f
vickyjaiswal647 <vickyjaiswal647@gmail.com>
2021-02-19 13:04:21 +0530

    Simple React WebApp

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Cards';
+import './index.css';
+import Sdata from './Sdata';
+
+const App =() => (
+  <>
+    <h1 className="heading_style">List of Top 5 Netflix Series in 2020</h1>
+    {Sdata.map((val) => {
+        return(
+            <Card key={val.id}
+                  imgsrc={val.imgscr}
+                  title={val.title}
+                  sname={val.sname}
+                  link={val.links}
+            />
+          );
+    })}
+  </>
+)
+export default App;
---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        id: 1,
+        imgscr:"https://wallpapercave.com/wp/wp6799259.jpg",
+        title:"A Netflix Original Series",
+        sname:"DARK",
+        links:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        id: 2,
+        imgscr:"https://wallpapercave.com/wp/wp7418550.jpg",
+        title:"A Netflix Original Series",
+        sname:"Extra Curricular",
+        links:"https://www.netflix.com/in/title/80990668?source:35"
+    },
+    {
+        id: 3,
+        imgscr:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        sname:"Stranger Things",
+        links:"https://www.netflix.com/in/title/80057281"
+    },
+    {
+        id: 1,
+        imgscr:"https://wallpapercave.com/wp/wp6799259.jpg",
+        title:"A Netflix Original Series",
+        sname:"DARK",
+        links:"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        id: 3,
+        imgscr:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        sname:"Stranger Things",
+        links:"https://www.netflix.com/in/title/80057281"
+    }
+    
+]
+
+export default Sdata;
